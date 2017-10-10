61fe9b46418dc667e54a3bbe1298d88e53855c2a
shen.zhibing <shen.zhibing@mobcb.com>
2017-10-10 19:39:25 +0800

    Add test module.

---
+++ throwable.retry.test/src/main/java/com/iylyi/throwable/retry/test/TestApp.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 - 2017. MOBCB Technology Co.,Ltd. All rights Reserved.
+ */
+
+package com.iylyi.throwable.retry.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * TestApp: Description of TestApp.
+ *
+ * @author shen.zhibing
+ * @version 1.00
+ * @since 2017-10-10 15:55
+ */
+@SpringBootApplication
+@ComponentScan(value = "com.iylyi")
+public class TestApp implements CommandLineRunner {
+    private TestService testService;
+
+    @Autowired
+    public TestApp(TestService testService) {
+        this.testService = testService;
+    }
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+        testService.retry();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestApp.class, args);
+    }
+}
---
+++ throwable.retry.test/src/main/java/com/iylyi/throwable/retry/test/TestService.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2014 - 2017. MOBCB Technology Co.,Ltd. All rights Reserved.
+ */
+
+package com.iylyi.throwable.retry.test;
+
+import com.iylyi.throwable.retry.annotation.Retry;
+import org.springframework.stereotype.Service;
+
+/**
+ * TestService: Description of TestService.
+ *
+ * @author shen.zhibing
+ * @version 1.00
+ * @since 2017-10-10 19:32
+ */
+@Service
+public class TestService {
+
+    @Retry(confirm = true)
+    public void retry() throws Exception {
+        Thread.sleep(3000);
+        throw new Exception("测试异常信息");
+    }
+}
