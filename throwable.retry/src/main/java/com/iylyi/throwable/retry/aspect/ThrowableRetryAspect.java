1abb38501455b8a7f0ac97401321958d7f068d0e
shen.zhibing <shen.zhibing@mobcb.com>
2017-12-12 17:36:38 +0800

    Add exclude exception control.

--- throwable.retry/src/main/java/com/iylyi/throwable/retry/aspect/ThrowableRetryAspect.java
+++ throwable.retry/src/main/java/com/iylyi/throwable/retry/aspect/ThrowableRetryAspect.java
@@ -74,7 +74,16 @@ public class ThrowableRetryAspect {
                     LOGGER.info("【" + pjp.getSignature().toString() + "】任务第(" + (attempts - 1) + "/" + maxAttempts + ")次重试失败!!!");
                 }
                 ta = throwable;
-                if (anno.retryFor().isInstance(ta)) {
+                boolean excluded = false;
+                for (Class clz : anno.excludes()) {
+                    if (clz.isInstance(ta)) {
+                        // 检查异常是否排除
+                        excluded = true;
+                        break;
+                    }
+                }
+
+                if (anno.retryFor().isInstance(ta) && !excluded) {
                     if (retry && attempts <= maxAttempts) { // 确认重试且重试次数小于最大重试次数时进行任务重试，否则直接抛出异常
                         LOGGER.info("【" + pjp.getSignature().toString() + "】任务执行第(" + attempts + "/" + maxAttempts + ")次重试...");
                     } else {
